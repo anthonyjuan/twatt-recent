e713404731045b9c1fb227f6576ce12169f792fc
Anthony Juan <anthonyjuan95@gmail.com>
2017-03-30 11:20:54 +0700

    add dotenv

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const oauth = require('oauth');
+require('dotenv').config();
 
 var myOauth = new oauth.OAuth(
+  //using CONSUMER KEY
   'https://api.twitter.com/oauth/request_token',
   'https://api.twitter.com/oauth/access_token',
-  'AHgDiLQRKUkWaV5tVmHWueHuO',
-  'JDcmmA00BSuLvb3BDbvIxb5Q8K3kfgfa65rYDEXZbJKCHaf8EB',
+  process.env.CONSUMER_KEY,
+  process.env.CONSUMER_SECRET,
   '1.0',
   'http://127.0.0.1:3000/twitter/callback',
   'HMAC-SHA1',
@@ -15,9 +17,10 @@ var myOauth = new oauth.OAuth(
 
 app.get('/',(req , res) => {
   myOauth.get(
-        'https://api.twitter.com/1.1/statuses/home_timeline.json',
-        '88395371-5g4YHhiLsUOdxMz8EkE99CxtcVxGXOO40UmgrXSLG', //test user token
-        'tvOIvXXxTyyVFJJ4nPg64OHzeEUmxgCW0sQugF3TFt2Lf', //test user secret
+    //using TOKEN
+        'https://api.twitter.com/1.1/statuses/home_timeline.json?count=3',
+        process.env.TOKEN, //test user token
+        process.env.TOKEN_SECRET, //test user secret
         function(e, data) {
           if (e) console.error(e);
           res.send(data);
@@ -30,9 +33,10 @@ app.get('/',(req , res) => {
 
 app.get('/user',(req , res) => {
   myOauth.get(
-        'https://api.twitter.com/1.1/statuses/user_timeline.json',
-        '88395371-5g4YHhiLsUOdxMz8EkE99CxtcVxGXOO40UmgrXSLG', //test user token
-        'tvOIvXXxTyyVFJJ4nPg64OHzeEUmxgCW0sQugF3TFt2Lf', //test user secret
+        'https://api.twitter.com/1.1/statuses/user_timeline.json?count=3', //user timeline
+        // 'https://api.twitter.com/1.1/statuses/home_timeline.json', //home timeline
+        process.env.TOKEN, //test user token
+        process.env.TOKEN_SECRET, //test user secret
         function(e, data) {
           if (e) console.error(e);
           res.send(data);
